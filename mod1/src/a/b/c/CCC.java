1bb8b88846d43f259397687d3fadbb39a03b4a6c
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2010-12-30 17:39:50 +0300

    branch1: comments changed

--- mod1/src/a/b/c/CCC.java
+++ mod1/src/a/b/c/CCC.java
@@ -6,6 +6,7 @@ package a.b.c;
 
 /**
  * by Irina.Petrovskaya, on 30.12.10, 17:23
+ * branch1
  */
 public class CCC {
 }
--- mod2/src/a/b/c/BBB.java
+++ mod2/src/a/b/c/BBB.java
@@ -6,6 +6,7 @@ package a.b.c;
 
 /**
  * by Irina.Petrovskaya, on 30.12.10, 17:04
+ * branch1
  */
 public class BBB {
 }
